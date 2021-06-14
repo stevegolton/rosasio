55d35f74c7a627dedae57cbf8fae45bf7fdbf6b9
Steve Golton <stevegolton@gmail.com>
2021-06-14 09:49:52 +0100

    Close subscriber connections when a publisher goes out of scope

--- include/rosasio/publisher.hpp
+++ include/rosasio/publisher.hpp
@@ -11,23 +11,53 @@
 
 namespace rosasio
 {
-    template <class SubscriberType>
+    template <class SubscriberType, class MsgType>
     class SubscriberPool
     {
     public:
+        SubscriberPool(bool latched)
+            : m_latched(latched),
+              m_has_latched_msg(false),
+              open(true) {}
+
+        void close()
+        {
+            for (auto conn : subscriber_connections)
+            {
+                conn->close();
+            }
+            open = false;
+        }
+
+        void publish(const MsgType &msg)
+        {
+            for (auto conn : subscriber_connections)
+            {
+                conn->publish(msg);
+            }
+
+            if (m_latched)
+            {
+                m_latched_msg = msg;
+                m_has_latched_msg = true;
+            }
+        }
+
         std::set<std::shared_ptr<SubscriberType>> subscriber_connections;
+        bool open;
+        bool m_latched;
+        bool m_has_latched_msg;
+        MsgType m_latched_msg;
     };
 
     template <class MsgType>
     class SubscriberConnection : public std::enable_shared_from_this<SubscriberConnection<MsgType>>
     {
     public:
-        SubscriberConnection(boost::asio::ip::tcp::socket &&sock, std::string node_name, SubscriberPool<SubscriberConnection<MsgType>> &pool, bool latched, MsgType &latched_msg)
+        SubscriberConnection(boost::asio::ip::tcp::socket &&sock, std::string node_name, std::weak_ptr<SubscriberPool<SubscriberConnection<MsgType>, MsgType>> pool)
             : m_sock(std::move(sock)),
               m_node_name(node_name),
-              m_pool(pool),
-              m_latched(latched),
-              m_latched_msg(latched_msg)
+              m_pool(pool)
         {
         }
 
@@ -59,6 +89,12 @@ namespace rosasio
             }
         }
 
+        void close()
+        {
+            std::cout << "Closing socket!\n";
+            m_sock.close();
+        }
+
         boost::asio::ip::tcp::socket m_sock;
 
     private:
@@ -81,27 +117,36 @@ namespace rosasio
             if (ec)
                 return;
 
-            using namespace std::placeholders;
-
-            boost::asio::async_read(m_sock,
-                                    boost::asio::buffer(&m_msglen, sizeof(m_msglen)),
-                                    std::bind(&SubscriberConnection::handle_close, this->shared_from_this(), _1, _2));
-
-            Message msg;
-            msg.add_field("message_definition=string-data\n\n");
-            msg.add_field("callerid", m_node_name);
-            msg.add_field("md5sum", ros::message_traits::MD5Sum<MsgType>::value());
-            msg.add_field("type", ros::message_traits::DataType<MsgType>::value());
-            msg.add_field("latching", m_latched ? "1" : "0");
-            msg.finish();
-
-            boost::asio::write(m_sock, boost::asio::buffer(msg.buf));
-            std::cout << "Adding subscriber connection\n";
-            m_pool.subscriber_connections.insert(this->shared_from_this());
-            
-            if (m_latched)
+            if (auto pool = m_pool.lock())
             {
-                publish(m_latched_msg);
+                using namespace std::placeholders;
+
+                Message msg;
+                msg.add_field("message_definition=string-data\n\n");
+                msg.add_field("callerid", m_node_name);
+                msg.add_field("md5sum", ros::message_traits::MD5Sum<MsgType>::value());
+                msg.add_field("type", ros::message_traits::DataType<MsgType>::value());
+                msg.add_field("latching", pool->m_latched ? "1" : "0");
+                msg.finish();
+
+                boost::asio::write(m_sock, boost::asio::buffer(msg.buf));
+                
+                std::cout << "Adding subscriber connection\n";
+                if (pool->open)
+                {
+                    pool->subscriber_connections.insert(this->shared_from_this());
+
+                    // TODO only publish lateched message if we actually have one to publish!
+                    if (pool->m_latched && pool->m_has_latched_msg)
+                    {
+                        publish(pool->m_latched_msg);
+                    }
+
+                    // Start another read
+                    boost::asio::async_read(m_sock,
+                                            boost::asio::buffer(&m_msglen, sizeof(m_msglen)),
+                                            std::bind(&SubscriberConnection::handle_close, this->shared_from_this(), _1, _2));
+                }
             }
         }
 
@@ -111,8 +156,11 @@ namespace rosasio
 
             if (ec == boost::asio::error::eof)
             {
-                m_pool.subscriber_connections.erase(this->shared_from_this());
-                std::cout << "Connection closed, erasing subscriber connection\n";
+                if (auto pool = m_pool.lock())
+                {
+                    std::cout << "Connection closed, erasing subscriber connection\n";
+                    pool->subscriber_connections.erase(this->shared_from_this());
+                }
             }
             else
             {
@@ -125,9 +173,7 @@ namespace rosasio
         uint32_t m_msglen;
         std::vector<uint8_t> m_buffer;
         std::string m_node_name;
-        SubscriberPool<SubscriberConnection<MsgType>> &m_pool;
-        bool m_latched;
-        MsgType &m_latched_msg;
+        std::weak_ptr<SubscriberPool<SubscriberConnection<MsgType>, MsgType>> m_pool;
     };
 
     template <class MsgType>
@@ -137,30 +183,23 @@ namespace rosasio
         Publisher(rosasio::Node &node, const std::string &topic_name, bool latched = false)
             : m_node(node),
               m_topic_name(topic_name),
-              m_acceptor(m_node.get_ioc(), boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 0)),
-              m_latched(latched)
+              m_acceptor(m_node.get_ioc(), boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 0))
         {
             boost::asio::ip::tcp::endpoint le = m_acceptor.local_endpoint();
             m_node.register_publisher<MsgType>(topic_name, le.port());
+            m_pool = std::make_shared<SubscriberPool<SubscriberConnection<MsgType>, MsgType>>(latched);
             start_accept();
         }
 
         virtual ~Publisher()
         {
             m_node.unregister_publisher<MsgType>(m_topic_name);
+            m_pool->close();
         }
 
         virtual void publish(const MsgType &msg)
         {
-            for (auto conn : m_pool.subscriber_connections)
-            {
-                conn->publish(msg);
-            }
-
-            if (m_latched)
-            {
-                m_latched_msg = msg;
-            }
+            m_pool->publish(msg);
         }
 
     private:
@@ -169,7 +208,7 @@ namespace rosasio
             boost::asio::ip::tcp::socket sock(m_node.get_ioc());
 
             // TODO we don't really need to move here - just create the socket as part of the connection object amd pass in ref to ioc
-            auto conn = std::make_shared<SubscriberConnection<MsgType>>(std::move(sock), m_node.get_name(), m_pool, m_latched, m_latched_msg);
+            auto conn = std::make_shared<SubscriberConnection<MsgType>>(std::move(sock), m_node.get_name(), m_pool);
 
             m_acceptor.async_accept(conn->m_sock,
                                     std::bind(
@@ -193,8 +232,6 @@ namespace rosasio
         std::string m_topic_name;
         boost::asio::ip::tcp::acceptor m_acceptor;
         std::function<void(const MsgType &)> m_cb;
-        SubscriberPool<SubscriberConnection<MsgType>> m_pool;
-        bool m_latched;
-        MsgType m_latched_msg;
+        std::shared_ptr<SubscriberPool<SubscriberConnection<MsgType>, MsgType>> m_pool;
     };
 } // namespace rosasio
